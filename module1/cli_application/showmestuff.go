989c57ec266021199034b0a3c332fc0711a50a9a
Ricardo Silva <ricardo.silva@mambu.com>
2022-02-11 16:29:55 +0100

    Working with reading from os.Stdin

--- module1/cli_application/showmestuff.go
+++ module1/cli_application/showmestuff.go
@@ -3,9 +3,15 @@ package main
 import (
 	"runtime"
 	"fmt"
+	"os"
+	"bufio"
 )
 
 func main(){
 	// this is where stuff starts to happen
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Whats your name?")
+	text, _ := reader.ReadString('\n')
+	fmt.Printf("Hello %v", text)
 	fmt.Printf("Go current version is %v running in %v. Sweet!!\n", runtime.Version(), runtime.GOOS)
 }
