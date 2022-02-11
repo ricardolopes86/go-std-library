69791e874285c1370c0dbbe2265775557670e397
Ricardo Silva <ricardo.silva@mambu.com>
2022-02-11 22:16:22 +0100

    An even more complex logger.

--- module3/firstlog/firstlog.go
+++ module3/firstlog/firstlog.go
@@ -5,12 +5,39 @@ import (
 	"os"
 )
 
+type messageType int
+
+const (
+	INFO messageType = 0 + iota
+	WARN
+	ERR
+	FATAL
+)
+
 func main(){
+	writeLog(WARN, "our message in the logs")
+}
+
+func writeLog(messageType messageType, message string) {
 	file, err := os.OpenFile("log.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.SetOutput(file)
-	log.Println("First log message!")
+
+	switch messageType {
+	case INFO:
+		logger := log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+		logger.Println(message)
+	case WARN:
+		logger := log.New(file, "WARN: ", log.Ldate|log.Ltime|log.Lshortfile)
+		logger.Println(message)
+	case ERR:
+		logger := log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+		logger.Println(message)
+	case FATAL:
+		logger := log.New(file, "FATAL: ", log.Ldate|log.Ltime|log.Lshortfile)
+		logger.Fatal(message)
+	}
 }
